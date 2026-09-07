c39e120443a55ee0320d902c3e14842085231d0a
TLarsh <adeyemisam2015@gmail.com>
2026-09-07 13:11:20 +0100

    feat(auth): restore getMe function and improve error handling in protect middleware

--- server/middlewares/auth.ts
+++ server/middlewares/auth.ts
@@ -1,5 +1,5 @@
 import {NextFunction, Request, Response} from "express";
-import {IUser} from "../models/User.js";
+import {IUser, User} from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 
@@ -11,21 +11,24 @@ export const protect = (req: AuthenticatedRequest, res: Response, next: NextFunc
 Promise<void> => {
     let token: string | undefined;
     if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-        token = req.headers.authorization.split(" ")[1];
-    }
+        try {
+            //Get token from header
+            token = req.headers.authorization.split(" ")[1];
+            //Verify token
+            const decoded = jwt.verify(token, process.env.JWT_SECRET) as {id: string};
+            //Get user from token exclude password
+            const user = await User.findById(decoded.id).select('-password');
+            if (!user) {
+                res.status(401).json({message: "Not authorized,user not found"});
+                return;
+            }
+            req.user = user;
+            next()
+        } catch (error) {
+            res.status(401).json({message: "Not authorized, token failed"});
+            return;
+        }
 
-    if (!token) {
-        res.status(401).json({message: "Not authorized, no token"});
-        return;
-    }
-
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {id: string};
-        req.user = { _id: decoded.id } as IUser;
-        next();
-    } catch (error) {
-        res.status(401).json({message: "Not authorized, token failed"});
-    }
-
-    
+            
+    }    
 }
